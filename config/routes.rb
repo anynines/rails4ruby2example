8aaf8b76a0d9228d2f3b2d1a5fcdd7bd6be93071
Julian Weber <jweber@avarteq.de>
2014-04-03 17:05:10 +0200

    adds root routing and posts scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails4ruby2example::Application.routes.draw do
+  resources :posts
+  root to: "posts#index"
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
